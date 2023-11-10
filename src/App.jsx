2379c63026956b876f8fc72ef928fcdfb8104902
amin86yehego <amin@yehego.com>
2023-11-10 23:33:20 +0100

    disable right click on app and on images!

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import "./App.css";
 import Nav from "./components/Nav";
+
+const disableRightClick = (event) => {
+  event.preventDefault();
+};
+
 function App() {
+  useEffect(() => {
+    document.addEventListener("contextmenu", disableRightClick);
+
+    return () => {
+      document.removeEventListener("contextmenu", disableRightClick);
+    };
+  }, []);
+
   return (
     <>
       <BrowserRouter>
-      <Nav/>
+        <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
--- src/components/HandyCrafts/index.jsx
+++ src/components/HandyCrafts/index.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import "./handycrafts.scss";
 import { handyImages } from "../../utils/data";
 
+const disableRightClick = (event) => {
+  event.preventDefault();
+};
+
 const HandyCrafts = () => {
   return (
     <div className="handicrafts-container">
       {handyImages.map((item, id) => {
         return (
           <div key={id} className="handicrafts-container_image">
-            <img src={item.url} alt={item.alt} />
+            <img
+              src={item.url}
+              alt={item.alt}
+              onContextMenu={disableRightClick}
+            />
           </div>
         );
       })}
