0b6f5bd6d8dd34945005e9413b8216db4608bc21
amin86yehego <amin@yehego.com>
2023-11-11 18:31:25 +0100

    update footer and add whatsapp

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,30 +3,51 @@ import Linkedin from "../SVG/Linkedin";
 import Gmail from "../SVG/Gmail";
 import Instagram from "../SVG/Instagram";
 import { Link } from "react-router-dom";
+import Telegram from "../SVG/Telegram";
+import Whatsapp from "../SVG/Whatsapp";
 const index = () => {
   return (
     <footer>
-      <Link
-        to={"https://www.instagram.com/art.maryam.fadaei"}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Instagram />
-      </Link>
-      <Link
-        to={"mailto:art.maryam.fadaei@gmail.com"}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Gmail />
-      </Link>
-      <Link
-        to={"https://www.linkedin.com/in/maryam-fadaei77"}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Linkedin />
-      </Link>
+      <div className="social-media-container">
+        <Link
+          to={"https://api.whatsapp.com/send?phone=46760200726"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Whatsapp />
+        </Link>
+        <Link
+          to={"https://www.instagram.com/art.maryam.fadaei"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Instagram />
+        </Link>
+        <Link
+          to={"mailto:art.maryam.fadaei@gmail.com"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Gmail />
+        </Link>
+        <Link
+          to={"https://www.linkedin.com/in/maryam-fadaei77"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Linkedin />
+        </Link>
+      </div>
+      <p className="amin-content">
+        Website designed and developed by
+        <Link
+          to={"mailto:aminhaghi@gmail.com"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Amin Haghi
+        </Link>
+      </p>
     </footer>
   );
 };
